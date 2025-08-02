f28e7fc5f7de2e6bdffa836b27ea262590e51db1
Leon1777 <168653957+Leon1777@users.noreply.github.com>
2025-08-03 00:46:30 +0200

    avoid crash on origin

--- consensus/src/processes/ghostdag/protocol.rs
+++ consensus/src/processes/ghostdag/protocol.rs
@@ -100,7 +100,10 @@ impl<T: GhostdagStoreReader, S: RelationsStoreReader, U: ReachabilityService, V:
     pub fn find_selected_parent(&self, parents: impl IntoIterator<Item = Hash>) -> Hash {
         parents
             .into_iter()
-            .map(|parent| SortableBlock { hash: parent, blue_work: self.ghostdag_store.get_blue_work(parent).unwrap() })
+            .map(|parent| SortableBlock {
+                hash: parent,
+                blue_work: self.ghostdag_store.get_blue_work(parent).unwrap_or(karlsen_math::Uint192([0, 0, 0])),
+            })
             .max()
             .unwrap()
             .hash
